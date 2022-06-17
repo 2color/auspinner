6e2473cd192626f81fcdcf681694323112902ce2
Daniel N <2color@users.noreply.github.com>
2022-06-17 21:20:23 +0200

    Connect to delegates and pass the origins
    It works with Estuary!!!!

--- main.go
+++ main.go
@@ -6,10 +6,24 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	pinclient "github.com/ipfs/go-pinning-service-http-client"
+
 	"github.com/ipld/go-car/v2"
+	"github.com/ipld/go-car/v2/blockstore"
+	"github.com/ipld/go-car/v2/index"
+
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/peer"
+	routinghelpers "github.com/libp2p/go-libp2p-routing-helpers"
+	"github.com/multiformats/go-multiaddr"
+	"github.com/multiformats/go-multicodec"
+
 	"github.com/urfave/cli/v2" // imports as package "cli"
+
+	"github.com/ipfs/go-bitswap"
+	bsnet "github.com/ipfs/go-bitswap/network"
 )
 
 // TODO: Find a cleaner way to handle default endpoints without duplicating
@@ -30,7 +44,6 @@ var tokenFlag = &cli.StringFlag{
 
 func main() {
 	var name string
-	var carFilePath string
 
 	// TODO: move pinning to sub command
 	app := &cli.App{
@@ -52,11 +65,10 @@ func main() {
 
 					pinClient := pinclient.NewClient(servicesEndpoints[c.String(serviceFlag.Name)], c.String(tokenFlag.Name)) // instantiate client with token
 
-					pinnedCids, err := listPins(c.Context, *pinClient)
+					_, err := listPins(c.Context, *pinClient)
 					if err != nil {
 						return err
 					}
-					fmt.Printf("pinned CIDs: %v \n", pinnedCids)
 
 					return nil
 				},
@@ -76,6 +88,7 @@ func main() {
 						log.Fatal("services should be one of: ", strings.Join(validServices, ", "))
 					}
 
+					var carFilePath string
 					if carFilePath = c.Args().First(); carFilePath == "" {
 						log.Fatal(".car file is required")
 					}
@@ -90,30 +103,84 @@ func main() {
 						return err
 					}
 
-					roots, err := r.Roots()
-					fmt.Printf("pinning CID: %v to service: %v\n", roots, c.String(serviceFlag.Name))
+					pinClient := pinclient.NewClient(servicesEndpoints[c.String(serviceFlag.Name)], c.String(tokenFlag.Name)) // instantiate client with token
+
+					// Create libp2p host
+					host, err := libp2p.New(
+						libp2p.NATPortMap(),
+						libp2p.EnableHolePunching(),
+					)
 					if err != nil {
 						return err
 					}
 
-					pinClient := pinclient.NewClient(servicesEndpoints[c.String(serviceFlag.Name)], c.String(tokenFlag.Name)) // instantiate client with token
-
-					for _, cid := range roots {
-						fmt.Printf("pinning root CID: %v", roots)
-						pinStatus, err := pinClient.Add(c.Context, cid)
-						if err != nil {
-							return err
+					// Create libp2p host
+					var mas []multiaddr.Multiaddr
+					var pinStatuses []pinclient.PinStatusGetter
+					// wait 10 seconds so port mapping has time to get set up
+					time.AfterFunc(time.Second*10, func() {
+						addr := peer.AddrInfo{
+							ID:    host.ID(),
+							Addrs: host.Addrs(),
 						}
-						fmt.Printf("pin status: %v", pinStatus)
+						mas, err = peer.AddrInfoToP2pAddrs(&addr)
 
+						if err != nil {
+							fmt.Println(err)
+							panic(err)
+						}
+
+						fmt.Println("multiaddrs:")
+						for _, a := range mas {
+							fmt.Println(a)
+						}
+
+						pinStatuses, err = addPins(c.Context, *pinClient, r, mas)
+
+						for _, d := range pinStatuses[0].GetDelegates() {
+							p, err := peer.AddrInfoFromP2pAddr(d)
+							if err != nil {
+								panic(err)
+							}
+
+							if err := host.Connect(c.Context, *p); err != nil {
+								log.Fatalf("error connecting to remote pin delegate %v : %v", d, err)
+							}
+						}
+
+						if err != nil {
+							fmt.Println(err)
+							panic(err)
+						}
+					})
+
+					bsopts := []bitswap.Option{
+						bitswap.EngineBlockstoreWorkerCount(600),
+						bitswap.TaskWorkerCount(600),
+						bitswap.MaxOutstandingBytesPerPeer(int(5 << 20)),
 					}
-					// endpoint, err := normalizeEndpoint(servicesEndpoints[svc])
-					// if err != nil {
-					// 	return err
-					// }
 
-					// fmt.Printf("pinning CID to service: %v", svc)
-					// <-c.Done()
+					robs, err := getCarBlockstore(r)
+					if err != nil {
+						return err
+					}
+
+					// Create a Bitswap server. To connect use the
+					bswap := bitswap.New(c.Context, // Make a new Bitswap server (actually it's both a client and a server, but for now you only care about the server aspect)
+						bsnet.NewFromIpfsHost( // There's some abstraction layer here and bad naming, but basically it's asking for pieces it need
+							host,                   // libp2p host used for communicating with others
+							&routinghelpers.Null{}, // a routing system for finding content for the client (also it does this wacky thing where it advertises new blocks it learns about ... but don't worry about this)
+						),
+						robs,      // this is the blockstore that I'm willing to serve from
+						bsopts..., // some configuration options and tuning
+					)
+					_ = bswap
+
+					// TODO:
+					_ = pinStatuses
+					// watchPinStatus()
+
+					<-c.Done()
 					return nil
 				},
 			},
@@ -125,6 +192,89 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+func getCarBlockstore(r *car.Reader) (*blockstore.ReadOnly, error) {
+	var idx index.Index
+	backingReader := r.DataReader()
+
+	if r.Version == 1 || !r.Header.HasIndex() {
+		idx, err := index.New(multicodec.CarMultihashIndexSorted)
+		if err != nil {
+			return nil, err
+		}
+		// TODO: Read more about how LoadIndex works for car files without an index
+		if err := car.LoadIndex(idx, r.DataReader()); err != nil {
+			return nil, err
+		}
+
+		// TODO: Save newly created index somewhere
+	} else {
+		i, err := index.ReadFrom(r.IndexReader())
+		if err != nil {
+			return nil, err
+		}
+		if i.Codec() != multicodec.CarMultihashIndexSorted {
+			return nil, fmt.Errorf("codec %s not supported for CAR files", i.Codec())
+		}
+		idx = i
+	}
+
+	return blockstore.NewReadOnly(backingReader, idx)
+}
+
+func addPins(ctx context.Context, client pinclient.Client, car *car.Reader, origins []multiaddr.Multiaddr) ([]pinclient.PinStatusGetter, error) {
+	// TODO: Figure out how a CAR file can have multiple root CIDs
+	roots, err := car.Roots()
+	fmt.Printf("pinning CID: %v\n", roots)
+	if err != nil {
+		return nil, err
+	}
+
+	pinStatuses := []pinclient.PinStatusGetter{}
+	opts := []pinclient.AddOption{}
+	for _, cid := range roots {
+		fmt.Printf("pinning root CID: %v", roots)
+		// TODO: How do I only pass publicly reachable addresses?
+		opts = append(opts, pinclient.PinOpts.WithOrigins(origins...)) // Pass the address so that the pinning service can fetch the
+		pinStatus, err := client.Add(ctx, cid, opts...)
+		if err != nil {
+			return nil, err
+		}
+		pinStatuses = append(pinStatuses, pinStatus)
+		fmt.Printf("status %v", pinStatus)
+	}
+	fmt.Printf("pinned %d CIDs", len(pinStatuses))
+
+	return pinStatuses, nil
+}
+
+// func connectToDelegates(ctx context.Context, h host.Host, delegates []string) error {
+// 	peers := make(map[peer.ID][]multiaddr.Multiaddr)
+// 	for _, d := range delegates {
+// 		ai, err := peer.AddrInfoFromString(d)
+// 		if err != nil {
+// 			return err
+// 		}
+
+// 		peers[ai.ID] = append(peers[ai.ID], ai.Addrs...)
+// 	}
+
+// 	for p, addrs := range peers {
+// 		h.Peerstore().AddAddrs(p, addrs, time.Hour)
+
+// 		if h.Network().Connectedness(p) != network.Connected {
+// 			if err := h.Connect(ctx, peer.AddrInfo{
+// 				ID: p,
+// 			}); err != nil {
+// 				return err
+// 			}
+
+// 			h.ConnManager().Protect(p, "pinning")
+// 		}
+// 	}
+
+// 	return nil
+// }
 
 func isServiceValid(svc string) bool {
 	for _, validSvc := range validServices {
@@ -139,9 +289,10 @@ func listPins(ctx context.Context, c pinclient.Client) ([]string, error) {
 	psCh, errCh := c.Ls(ctx, pinclient.PinOpts.FilterStatus(pinclient.StatusPinned, pinclient.StatusPinning, pinclient.StatusFailed, pinclient.StatusQueued))
 	pinnedCids := []string{}
 
+	fmt.Println("CID | Request ID | Created | Status")
 	for ps := range psCh {
-		// pinnedCids = append(pinnedCids, ps.GetRequestId())
-		pinnedCids = append(pinnedCids, ps.GetPin().GetCid().String())
+		pinnedCids = append(pinnedCids, ps.GetRequestId())
+		fmt.Printf("%s %s (%s) %s\n", ps.GetPin().GetCid().String(), ps.GetRequestId(), ps.GetCreated().Format(time.RFC822), ps.GetStatus())
 	}
 
 	if err := <-errCh; err != nil {
@@ -151,6 +302,10 @@ func listPins(ctx context.Context, c pinclient.Client) ([]string, error) {
 }
 
 // TODO: only needed if the cli can take user provided endpoints, which it should for interoprability...
+// endpoint, err := normalizeEndpoint(servicesEndpoints[svc])
+// if err != nil {
+// 	return err
+// }
 // func normalizeEndpoint(endpoint string) (string, error) {
 // 	uri, err := url.ParseRequestURI(endpoint)
 // 	if err != nil || !(uri.Scheme == "http" || uri.Scheme == "https") {
